b320515e42968a0cdf3cc35f8bd8f8635dcb72c4
Gökay Öncü <128924810+gokayoncu@users.noreply.github.com>
2024-09-06 17:42:47 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,19 @@
+'use client'; 
 import React from 'react';
-import { fetchAllCoins } from './services';  // API çağrısı
+import { useQuery } from 'react-query';
+import { fetchAllCoins } from './services';
 import CryptoCard from './components/CryptoCard';
 import Watchlist from './components/Watchlist';
 import { Coin } from './types/type';
 import { useCryptoContext } from './context/context';
 import styles from './styles/CryptoCard.module.css';
 
-interface PageProps {
-  coins: Coin[];
-}
-
-const Home: React.FC<PageProps> = async () => {
-  const coins = await fetchAllCoins();  // Server-side veri alımı
+const Home: React.FC = () => {
+  const { data: coins, error, isLoading } = useQuery<Coin[]>('coins', fetchAllCoins);
   const { watchlist, addToWatchlist, removeFromWatchlist } = useCryptoContext();
+
+  if (isLoading) return <p>Loading...</p>;
+  if (error) return <p>Error occurred!</p>;
 
   return (
     <div className={styles.container}>
@@ -21,31 +22,12 @@ const Home: React.FC<PageProps> = async () => {
         removeFromWatchlist={removeFromWatchlist} // Prop olarak geçirin
       />
       <div className={styles.cryptoList}>
-        {coins.map(coin => (
+        {coins?.map(coin => (
           <CryptoCard key={coin.id} coin={coin} onAddToWatchlist={addToWatchlist} />
         ))}
       </div>      
     </div>
   );
 };
-
-// SSR ile veri getirme (yalnızca veri getirme)
-export async function getServerSideProps() {
-  try {
-    const coins = await fetchAllCoins();
-    return {
-      props: {
-        coins,
-      },
-    };
-  } catch (error) {
-    console.error('Failed to fetch coins:', error);
-    return {
-      props: {
-        coins: [],
-      },
-    };
-  }
-}
 
 export default Home;
